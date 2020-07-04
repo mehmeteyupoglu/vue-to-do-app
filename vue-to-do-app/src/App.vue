370440c830b91e466adbbe47c06ef0b60ac4b074
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-07-04 19:55:09 +0300

    Initial commit

---
+++ vue-to-do-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ToDo todo="Create your Todo List!!!"/>
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-to-do-app/src/components/ToDo.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ todo }}</h1>
+    <form @submit.prevent="addTodo">
+      <input  v-model="todos" placeholder="Add Todos..."/>
+    </form>
+    
+    <div v-if="todoList.length > 0">
+      <ul>
+        <li v-for="item in todoList" :key="item" >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    todo: String
+  }, 
+
+  data(){
+    return {
+      todos :  "",
+      todoList: ["Mehmet", "Sena"] 
+
+    }
+  }, 
+  methods : {
+    addTodo() {
+      this.todoList.push(this.todos)
+      this.todos = ""
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
